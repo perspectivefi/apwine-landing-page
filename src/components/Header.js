3c7ece8613dd3a194d6e349fd94b30ba1b57fe2c
ulydev <ulydev@live.fr>
2020-07-19 17:08:23 +0200

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from "react"
 
-import logo from "../images/logo.png"
+import { Link } from "gatsby"
+
+import logo from "../images/logo_small.png"
 
 const Header = () => {
     return (
         <header className="flex flex-row items-center justify-center py-8 bg-blue-500">
 
-            <img src="https://placehold.it/250x250" className="w-16 h-16" />
+            <Link to="/"><img src={logo} className="w-16 h-16" /></Link>
 
         </header>
     )
