6174d2c8fed9350854a2fc4eab8a3fefc7fe22b8
ulydev <ulydev@live.fr>
2020-07-19 17:19:08 +0200

    set base info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Upstreamer`,
+    description: `Add fun, live interaction to any stream or video`,
+    author: `ulydev`,
+    baseUrl: `https://upstreamer.app`
   },
   plugins: [
     `gatsby-plugin-postcss`,
