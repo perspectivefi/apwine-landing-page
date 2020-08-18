5b608b987dadfe6cd4099f12430c95bc5ddd9483
ulydev <ulydev@live.fr>
2020-08-18 18:04:38 +0200

    switch to typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,11 @@ import React from "react"
 
 import Layout from "../components/Layout"
 
+import Hero from "../components/Hero"
+
 const IndexPage = () => (
     <Layout className="bg-white">
-        test
+        <Hero />
     </Layout>
 )
 
