64ddcee932c9c10c5ee5db2a23cb1aba494dabb2
ulydev <ulydev@live.fr>
2020-08-18 19:29:41 +0200

    site in construction message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,14 @@ import React from "react"
 import Layout from "../components/Layout"
 
 import Hero from "../components/Hero"
+import Container from "../components/Container"
 
 const IndexPage = () => (
     <Layout className="bg-white">
         <Hero />
+        <Container>
+            <h2 className="text-gray-800 text-lg font-normal text-center">Site currently being built. Join the discussion group for more updates!</h2>
+        </Container>
     </Layout>
 )
 
