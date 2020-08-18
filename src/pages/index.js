f5843e7a0fb23b09148fa21c667384610d290336
ulydev <ulydev@live.fr>
2020-08-18 17:43:54 +0200

    reinitialize index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,10 @@
 import React from "react"
 
 import Layout from "../components/Layout"
-import Wave from "../components/Wave"
-
-import Header from "../components/Header"
-import Hero from "../components/Hero"
-
-import Benefits from "../components/Benefits"
-import Usage from "../components/Usage"
-import Customisation from "../components/Customisation"
-
-import Contact from "../components/Contact"
-import Footer from "../components/Footer"
-
-import "../../node_modules/highlight.js/styles/xcode.css"
 
 const IndexPage = () => (
     <Layout className="bg-white">
-        <div className="min-h-screen flex flex-col">
-            <Header />
-            <Hero />
-            <Wave position="top" />
-        </div>
-
-        <Benefits />
-        <Usage />
-        <Customisation />
-
-        <Wave position="bottom" />
-        <Contact />
-        <Footer />
+        test
     </Layout>
 )
 
