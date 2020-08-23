58e9f0709392689b456bf451dd45723e7b5f95e4
ulydev <ulydev@live.fr>
2020-08-23 11:10:17 +0200

    add team icons

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -10,9 +10,15 @@ import gaspard from "../images/team/gaspard.jpg"
 import antoine from "../images/team/antoine.jpg"
 import ulysse from "../images/team/ulysse.jpg"
 
+import { FiTwitter, FiGlobe } from "react-icons/fi"
+
 const GradientBackground = styled.div`
     background: linear-gradient(to bottom left, #90cdf4, #4299e1);
 `
+
+const IconLink = ({ href, icon: Icon }) => (
+    <a href={href} target="_blank" className="ml-2 hover:text-blue-500"><Icon /></a>
+)
 
 const Person = ({ image, name, description }) => (
     <div className="flex flex-col md:flex-row mt-16 md:items-center">
@@ -22,7 +28,7 @@ const Person = ({ image, name, description }) => (
             </GradientBackground>
         </div>
         <div className="flex flex-col mt-2 md:mt-0 md:ml-4">
-            <h3 className="text-gray-800 text-2xl font-bold text-left">{ name }</h3>
+            <h3 className="flex flex-row items-center text-gray-800 text-2xl font-bold text-left">{ name }</h3>
             <Paragraph className="mt-2">{ description }</Paragraph>
         </div>
     </div>
@@ -31,9 +37,15 @@ const Person = ({ image, name, description }) => (
 const Team = () => (
     <div>
         <Title>Team</Title>
-        <Person image={gaspard} name="Gaspard Peduzzi" description="CS Student at EPFL, Gaspard co-founded the Blockchain Student Association. He is passionate about trustless/censorship resistant systems and actively conducting research in those fields." />
-        <Person image={antoine} name="Antoine Mouran" description="The uprise of Decentralised Finance (DeFi) marked a turning point for Antoine. This passion led him to become an active member and moderator of the DeFi France community and a member of the Blockchain Student Association at EPFL, where he's currently studying CS." />
-        <Person image={ulysse} name="Ulysse Ramage" description="Originally from Paris, Ulysse graduated in CS at EPFL and is now working as a software engineer at Amazon. He's interested in building user-friendly applications and interfaces." />
+        <Person image={gaspard} name={
+            <>Gaspard Peduzzi<IconLink href="https://twitter.com/GaspardPeduzzi" icon={FiTwitter} /></>
+        } description="CS Student at EPFL, Gaspard co-founded the Blockchain Student Association. He is passionate about trustless/censorship resistant systems and actively conducting research in those fields." />
+        <Person image={antoine} name={
+            <>Antoine Mouran<IconLink href="https://twitter.com/AntoineMouran" icon={FiTwitter} /></>
+        } description="The uprise of Decentralised Finance (DeFi) marked a turning point for Antoine. This passion led him to become an active member and moderator of the DeFi France community and a member of the Blockchain Student Association at EPFL, where he's currently studying CS." />
+        <Person image={ulysse} name={
+            <>Ulysse Ramage<IconLink href="https://uly.dev" icon={FiGlobe} /></>
+        } description="Originally from Paris, Ulysse graduated in CS at EPFL and is now working as a software engineer at Amazon. He's interested in building user-friendly applications and interfaces." />
     </div>
 )
 
