2a0673999ec504ec650571ec4142980ec9b2b609
ulydev <ulydev@live.fr>
2020-08-29 01:00:23 +0200

    rename BlogHeader to PageHeader

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -8,7 +8,7 @@ import Bubbles from "./Bubbles"
 
 import logo from "../images/logo_outline_small.png"
 
-const BlogHeader = () => (
+const PageHeader = ({ title = "APWine" }) => (
     <div className="bg-gray-900 flex flex-col relative">
         <Bubbles />
         <Container className="flex flex-row items-center justify-start w-full -mb-2 z-10">
@@ -17,7 +17,7 @@ const BlogHeader = () => (
                 <h2 className={classnames(
                     "text-3xl font-bold leading-tight text-white text-center"
                 )}>
-                    APWine Blog
+                    { title }
                 </h2>
                 <a href="/" className="text-sm text-gray-400 hover:text-blue-500 leading-tight">Back to <b>apwine.fi</b></a>
             </div>
@@ -26,4 +26,4 @@ const BlogHeader = () => (
     </div>
 )
 
-export default BlogHeader
+export default PageHeader
