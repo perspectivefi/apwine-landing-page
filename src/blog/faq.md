806895ff9c63b8022cf6a2ffe120f32734e75d54
ulydev <ulydev@live.fr>
2020-08-29 23:25:16 +0200

    update full faq

--- src/blog/faq.md
+++ src/blog/faq.md
@@ -9,3 +9,5 @@ Hello from the APWine team, and welcome to everyone who has joined the community
 Following the questions we've been asked since the public announcement two days ago, we have set up a more in-depth [Frequently Asked Questions](https://apwine.fi/faq) page on the website.
 
 As always, we are happy to hear your feedback and add any other clarifications you might think of.
+
+Stay tuned for more updates on the upcoming alpha and the APW token next week!
--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -9,20 +9,44 @@ import { FiPlus, FiMinus } from "react-icons/fi"
 
 const questions = [
     [
-        "What would be the interest to lock tokens inside your protocol?",
+        "What would be the point of locking tokens inside your protocol?",
         "You can instantly sell your unrealised APY to hedge against price movement."
     ],
     [
-        "Why would I be interested in the governance token?",
-        "The token will be useful to decide in which direction the project is going to go, and vote on particular functionalities. Token Holders also get as a reward 1% of the pool of every expiring \"contracts\"."
+        "What is the idea behind APWine, in one sentence?",
+        "The APWine protocol locks funds to generate interests which are tokenized as futures, enabling a DeFi user to trade unrealised yield."
     ],
     [
-        "What are the risks of locking my funds in your protocol?",
-        "The only risk to take into account is that of yield protocol SC failure."
+        "Will there be a token?",
+        "The APWine protocol will issue an APW token. Its mechanisms and distribution will be announced soon in an upcoming blog post."
     ],
     [
-        "Why are you building this project? What problem does it solve?",
-        "We want to bring a new DeFi tool to help farmers hedge their risk, while being able to farm with their own yield."
+        "Why is the bearing interest token locked for the period corresponding to the future?",
+        "Locking this token is necessary in order to generate the interest during the entire period. The possibility of withdrawing your fund during a future period will be added later."
+    ],
+    [
+        "How is it possible to trade our interest before it is even realised?",
+        "While locking the bearing interest tokens during the period corresponding to the future, the protocol mints a number of future interest tokens that correspond to the amount of funds locked by the user. This token is tradable and will allow its holder to redeem the yield gathered by our smart contract at the expiration of a future. Market actors can therefore speculate in the future value of the yield of an asset (for example a Dai deposit on Compound) by selling or buying this future interest token."
+    ],
+    [
+        "Where can we get those future interest tokens?",
+        "You can mint them by locking your funds on our platform for a period of a future. Or you can buy them on our exchange and everywhere they are traded."
+    ],
+    [
+        "What are the incentives for the APW token holders?",
+        "At each future expiration, 1% of the generated yield is shared between all APW token holders (percentage subject to change in the future based on the market demand). The token will also enable them to participate in the governance mechanisms of APWine."
+    ],
+    [
+        "What are some of the use cases?",
+        "You want to get involved in yield farming but the fluctuation of APYs scares you. So you want to sell your interests at a fixed price to minimise impact from fluctuations. For example, after having deposited your DAI on Compound you can lock your cDAI in APWine for a week and trade in advance the yield that they will generate this week."
+    ],
+    [
+        "What are the motivations behind the project? What problem does it solve?",
+        "We want to create a financial instrument to trade future interest rates in DeFi. Also helping the farmers to hedge their risks, and farm with their own yield."
+    ],
+    [
+        "Who is it for? Everybody? DeFi experts? The novices?",
+        "APWine includes an exchange where everyone will be able to trade future interests easily on it. We expect to get traction also from yield farmers, arbitrageurs, and traders."
     ]
 ]
 
