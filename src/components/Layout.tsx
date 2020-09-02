9257d30fcc3cebc37da0e06d2ec36748368404c0
ulydev <ulydev@live.fr>
2020-09-02 02:06:03 +0200

    add footer to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,15 @@
 import React, { FunctionComponent } from "react"
 
 import SEO from "./SEO"
+import Container from "./Container"
+import Footer from "./Footer"
 
 const Layout: FunctionComponent<{ pageName?: string, className?: string }> = ({ children, pageName, ...props }) => {
   return (
     <div {...props}>
       {children}
       <SEO pageName={pageName} />
+      <Container children={<Footer />} />
     </div>
   )
 }
