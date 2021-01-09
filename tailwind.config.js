5a0a098523b51e2a801b3bfc6b6ce04e3820ceb4
ulydev <ulydev@live.fr>
2021-01-09 17:14:36 +0100

    upgrade tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                "primary":          "#5B41F5",
+                "primary-light":    "#DAD6EF",
+                "primary-dark":     "#402CBB",
+                "black":            "#0F0F13"
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
