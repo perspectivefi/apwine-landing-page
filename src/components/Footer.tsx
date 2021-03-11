30ad4fde5d3fdf8ca2c1a0a4c123686053edf1c8
ulydev <ulydev@live.fr>
2021-03-11 02:57:08 +0100

    add Discord and documentation links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,71 @@
 import React from "react"
 
-import { FaGithub, FaMedium, FaMediumM, FaTelegram, FaTelegramPlane, FaTwitter } from "react-icons/fa"
+import {
+  FaDiscord,
+  FaGithub,
+  FaMedium,
+  FaMediumM,
+  FaTelegram,
+  FaTelegramPlane,
+  FaTwitter,
+} from "react-icons/fa"
 
 const Footer = () => (
-    <div className="flex flex-col">
-        <div className="flex flex-row items-center justify-between w-full max-w-xs text-primary-200 mx-auto">
-            <a href="https://github.com/APWine" target="_blank" rel="noopener noreferrer" className="hover:text-primary-100 transition duration-300"><FaGithub /></a>
-            <a href="https://twitter.com/APWineFinance" target="_blank" rel="noopener noreferrer" className="hover:text-primary-100 transition duration-300"><FaTwitter /></a>
-            <a href="https://apwine.medium.com" target="_blank" rel="noopener noreferrer" className="hover:text-primary-100 transition duration-300"><FaMediumM /></a>
-            <a href="https://t.me/APWineFinance" target="_blank" rel="noopener noreferrer" className="hover:text-primary-100 transition duration-300"><FaTelegramPlane /></a>
-        </div>
-        <div className="flex flex-row justify-between text-xs text-primary-100 mt-24 mx-auto md:mx-0">
-            <span>© APWine, 2021. All rights reserved.</span>
-            { /*
+  <div className="flex flex-col">
+    <div className="flex flex-row items-center justify-between w-full max-w-xs text-primary-200 mx-auto">
+      <a
+        href="https://github.com/APWine"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-primary-100 transition duration-300"
+      >
+        <FaGithub />
+      </a>
+      <a
+        href="https://twitter.com/APWineFinance"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-primary-100 transition duration-300"
+      >
+        <FaTwitter />
+      </a>
+      <a
+        href="https://apwine.medium.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-primary-100 transition duration-300"
+      >
+        <FaMediumM />
+      </a>
+      <a
+        href="https://t.me/APWineFinance"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-primary-100 transition duration-300"
+      >
+        <FaTelegramPlane />
+      </a>
+      <a
+        href="https://discord.gg/AxbH7sE6sc"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-primary-100 transition duration-300"
+      >
+        <FaDiscord />
+      </a>
+    </div>
+    <div className="flex flex-row justify-between text-xs text-primary-100 mt-24 mx-auto md:mx-0">
+      <span>© APWine, 2021. All rights reserved.</span>
+      {/*
             TODO: add later
             <div className="flex flex-row space-x-12">
                 <a href="">Legal</a>
                 <a href="">Terms of Use</a>
                 <a href="">Privacy Policy</a>
             </div>
-            */ }
-        </div>
+            */}
     </div>
+  </div>
 )
 
 export default Footer
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,20 +4,64 @@ import React from "react"
 import logo from "../images/logo_text.svg"
 
 const Header = () => (
-    <div className="flex flex-col space-y-8 md:space-y-0 md:flex-row px-6 md:px-10 py-8 md:py-10 justify-between">
-        <div className="flex flex-row items-center justify-between">
-            <Link to="/"><img src={logo} alt="logo" className="h-8 transform hover:scale-105 transition duration-300" /></Link>
-            <a href="https://app.apwine.fi" target="_blank" rel="noopener referrer" className="text-sm text-white px-4 py-2 border-2 border-primary-200 rounded-xl md:hidden transition duration-300 hover:text-white hover:bg-primary-200">Enter Beta</a>
-        </div>
-        <div className="flex flex-row items-center sm:space-x-12 text-sm justify-around sm:mx-auto md:mx-0">
-            <Link to="/" className="text-white hover:text-primary-200 transition duration-300">About</Link>
-            <a href="https://gov.apwine.fi" target="_blank" rel="noopener referrer" className="text-white hover:text-primary-200 transition duration-300">Forum</a>
-            { /* <Link href="/blog" className="text-white">Blog</a> */ }
-            <Link to="/faq" className="text-white hover:text-primary-200 transition duration-300">FAQ</Link>
-            <a href="#" className="text-white opacity-25">Documentation</a>
-            <a href="https://app.apwine.fi" target="_blank" rel="noopener referrer" className="text-white px-4 py-2 border-2 border-primary-200 rounded-xl hidden md:block transition duration-300 hover:text-white hover:bg-primary-200">Enter Beta</a>
-        </div>
+  <div className="flex flex-col space-y-8 md:space-y-0 md:flex-row px-6 md:px-10 py-8 md:py-10 justify-between">
+    <div className="flex flex-row items-center justify-between">
+      <Link to="/">
+        <img
+          src={logo}
+          alt="logo"
+          className="h-8 transform hover:scale-105 transition duration-300"
+        />
+      </Link>
+      <a
+        href="https://app.apwine.fi"
+        target="_blank"
+        rel="noopener referrer"
+        className="text-sm text-white px-4 py-2 border-2 border-primary-200 rounded-xl md:hidden transition duration-300 hover:text-white hover:bg-primary-200"
+      >
+        Enter Beta
+      </a>
     </div>
+    <div className="flex flex-row items-center sm:space-x-12 text-sm justify-around sm:mx-auto md:mx-0">
+      <Link
+        to="/"
+        className="text-white hover:text-primary-200 transition duration-300"
+      >
+        About
+      </Link>
+      <a
+        href="https://gov.apwine.fi"
+        target="_blank"
+        rel="noopener referrer"
+        className="text-white hover:text-primary-200 transition duration-300"
+      >
+        Forum
+      </a>
+      {/* <Link href="/blog" className="text-white">Blog</a> */}
+      <Link
+        to="/faq"
+        className="text-white hover:text-primary-200 transition duration-300"
+      >
+        FAQ
+      </Link>
+      <a
+        href="https://docs.apwine.fi"
+        target="_blank"
+        rel="noopener referrer"
+        className="text-white hover:text-primary-200 transition duration-300"
+      >
+        Documentation
+      </a>
+      <a
+        href="https://app.apwine.fi"
+        target="_blank"
+        rel="noopener referrer"
+        className="text-white px-4 py-2 border-2 border-primary-200 rounded-xl hidden md:block transition duration-300 hover:text-white hover:bg-primary-200"
+      >
+        Enter Beta
+      </a>
+    </div>
+  </div>
 )
 
 export default Header
