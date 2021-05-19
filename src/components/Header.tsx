14c6f8ebd547100b6b55919e3afafff15223a452
ulydev <ulydev@live.fr>
2021-05-19 13:13:16 +0200

    add enter LBP

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,12 +14,12 @@ const Header = () => (
         />
       </Link>
       <a
-        href="https://app.apwine.fi"
+        href="https://lbp.apwine.fi"
         target="_blank"
         rel="noopener referrer"
         className="px-4 py-2 text-sm text-white transition duration-300 border-2 border-primary-200 rounded-xl md:hidden hover:text-white hover:bg-primary-200"
       >
-        Enter Beta
+        Enter LBP
       </a>
     </div>
     <div className="flex flex-row items-center justify-around text-sm sm:space-x-12 sm:mx-auto md:mx-0">
@@ -61,12 +61,12 @@ const Header = () => (
         Careers
       </a>
       <a
-        href="https://app.apwine.fi"
+        href="https://lbp.apwine.fi"
         target="_blank"
         rel="noopener referrer"
         className="hidden px-4 py-2 text-white transition duration-300 border-2 border-primary-200 rounded-xl md:block hover:text-white hover:bg-primary-200"
       >
-        Enter Beta
+        Enter LBP
       </a>
     </div>
   </div>
