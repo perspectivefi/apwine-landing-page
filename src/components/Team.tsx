d5efe48452e14a50ec701ad0285e5b9c75e95af4
Jean Chambras <jean.chambras@epfl.ch>
2021-10-12 17:58:58 +0200

    Update team pictures

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -4,51 +4,129 @@ import styled from "styled-components"
 
 import classnames from "classnames"
 
-import gaspard from "../images/team/gaspard.jpg"
-import antoine from "../images/team/antoine.jpg"
+import gaspard from "../images/team/gaspard.png"
+import antoine from "../images/team/antoine.png"
 import ulysse from "../images/team/ulysse.jpg"
-import jean from "../images/team/jean.jpg"
-
+import jean from "../images/team/jean.png"
 import julien from "../images/team/julien.png"
-
 import jiahua from "../images/team/jiahua.jpg"
 import marc from "../images/team/marc.jpg"
+import mat from "../images/team/mat.jpeg"
+import red from "../images/team/red.jpeg"
+import sam from "../images/team/sam.jpeg"
+import dan from "../images/team/dan.jpeg"
+import ayush from "../images/team/ayush.jpeg"
+import daniel from "../images/team/daniel.png"
 
 import { FaTwitter } from "react-icons/fa"
 
 const Person = ({ img, name, twitter, description }) => (
-    <div className="relative w-28 lg:w-auto lg:flex-1 flex flex-col rounded-xl">
-        <img src={img} className="w-28 lg:w-28 rounded-t-xl" />
-        <div className="w-28 lg:w-28 flex flex-col p-2 bg-primary-300 bg-opacity-20 rounded-b-xl">
-            <div className="flex flex-row items-center justify-between">
-                <span className="text-white text-xs text-primary-100">{ name }</span>
-                <a href={`https://twitter.com/${twitter}`} target="_blank" rel="noopener referrer" className="hover:text-primary-100 transition duration-300 text-primary-200 text-sm"><FaTwitter /></a>
-            </div>
-            <span className="absolute bottom-0 left-0 right-0 text-center text-xs text-primary-200 -mb-5 opacity-75">{ description }</span>
-        </div>
+  <div className="w-28 flex flex-col rounded-xl">
+    <div className="w-28 h-28">
+      <img src={img} className="w-28 object-cover lg:w-28 rounded-t-xl" />
     </div>
+    <div className="w-28 flex flex-col p-2 bg-primary-300 bg-opacity-20 rounded-b-xl">
+      <div className="flex flex-row items-center justify-between">
+        <span className="text-white text-xs text-primary-100">{name}</span>
+        {twitter && (
+          <a
+            href={`https://twitter.com/${twitter}`}
+            target="_blank"
+            rel="noopener referrer"
+            className="hover:text-primary-100 transition duration-300 text-primary-200 text-sm"
+          >
+            <FaTwitter />
+          </a>
+        )}
+      </div>
+    </div>
+    <span className="text-center text-xs text-primary-200 opacity-75">
+      {description}
+    </span>
+  </div>
 )
 
 const Team = () => (
-    <div className="mx-auto flex flex-col items-start justify-center sm:w-full sm:max-w-2xl lg:max-w-none lg:w-auto sm:grid sm:grid-cols-2 lg:flex lg:flex-row space-y-16 sm:space-y-0 lg:space-x-16">
-        <div className="mx-auto lg:mx-0 flex flex-col space-y-2 w-64 lg:w-auto">
-            <span className="text-white text-xl font-bold">Advisors</span>
-            <div className="grid grid-cols-2 gap-10 lg:gap-0 lg:flex lg:flex-row lg:space-x-4">
-                <Person img={julien} name="Julien B." twitter="bneiluj" description="Stake Capital, Curve" />
-                <Person img={marc} name="Marc Z." twitter="lemiscate" description="Aave" />
-                <Person img={jiahua} name="Jiahua X." twitter="jiahuajavaxu" description="EPFL, UCL" />
-            </div>
-        </div>
-        <div className="mx-auto lg:mx-0 flex flex-col space-y-2 w-64 lg:w-auto">
-            <span className="text-white text-xl font-bold">Team</span>
-            <div className="grid grid-cols-2 gap-10 lg:gap-0 lg:flex lg:flex-row lg:space-x-4">
-                <Person img={jean} name="Jean C." twitter="jean_chambras" description="Comm, Branding" />
-                <Person img={gaspard} name="Gaspard P." twitter="gaspardpeduzzi" description="Protocol" />
-                <Person img={ulysse} name="Ulysse R." twitter="ulydev" description="UI / UX" />
-                <Person img={antoine} name="Antoine M." twitter="L0GYKAL" description="DeFi Evangelist" />
-            </div>
-        </div>
+  <div className="lg:flex lg:space-x-8 space-y-8 lg:space-y-0">
+    <div className="mx-auto lg:mx-0 flex flex-col space-y-2 w-64 lg:w-full">
+      <span className="text-white text-xl font-bold">Advisors</span>
+      <div className="grid grid-cols-2 gap-4 lg:grid-cols-3">
+        <Person
+          img={julien}
+          name="Julien B."
+          twitter="bneiluj"
+          description="Stake Capital, Curve"
+        />
+        <Person
+          img={marc}
+          name="Marc Z."
+          twitter="lemiscate"
+          description="Aave"
+        />
+        <Person
+          img={jiahua}
+          name="Jiahua X."
+          twitter="jiahuajavaxu"
+          description="EPFL, UCL"
+        />
+      </div>
     </div>
+
+    <div className="mx-auto lg:mx-0 flex flex-col space-y-2 w-64 lg:w-full">
+      <span className="text-white text-xl font-bold">Team</span>
+      <div className="grid grid-cols-2 gap-4 lg:grid-cols-3">
+        <Person
+          img={jean}
+          name="Jean C."
+          twitter="jean_chambras"
+          description="Full stack"
+        />
+        <Person
+          img={gaspard}
+          name="Gaspard P."
+          twitter="gaspardpeduzzi"
+          description="Protocol"
+        />
+        <Person
+          img={ulysse}
+          name="Ulysse R."
+          twitter="ulydev"
+          description="UI / UX"
+        />
+        <Person
+          img={antoine}
+          name="Antoine M."
+          twitter="L0GYKAL"
+          description="Integrations"
+        />
+        <Person
+          img={ayush}
+          name="Ayush T."
+          twitter="Ayush_XXX_"
+          description="Solidity"
+        />
+        <Person img={mat} name="Mat F." twitter="" description="Full Stack" />
+        <Person
+          img={daniel}
+          name="Daniel T."
+          twitter=""
+          description="Full Stack"
+        />
+        <Person
+          img={red}
+          name="Red"
+          twitter="smokatokey"
+          description="Marketing"
+        />
+        <Person
+          img={sam}
+          name="Sam"
+          twitter="Sam48152259"
+          description="Community"
+        />
+      </div>
+    </div>
+  </div>
 )
 
 export default Team
