08a3306d2c4baeea4f1f63dd7723dc4230d37081
Erikas <erikas@vsbl.lt>
2022-02-15 13:53:55 +0200

    Add new member

--- src/pages/Homepage/ourTeam.js
+++ src/pages/Homepage/ourTeam.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import React, { useRef, useEffect } from "react";
-import { Link } from "gatsby";
-import Lottie from "lottie-web";
-import PersonTile from "../../components/personTile";
-import * as animationData from "../../jsons/button_hover.json";
-=======
-import * as React from "react"
+import React, { useRef, useEffect } from "react"
 import PersonTile from "../../components/personTile"
 import { Link } from "gatsby"
->>>>>>> 45a0528367ddb358e3fccc2f355a0f5a5042541f
 
 import Team1 from "../../images/team/1.png"
 import Team2 from "../../images/team/2.png"
@@ -20,6 +12,7 @@ import Team7 from "../../images/team/7.png"
 import Team8 from "../../images/team/8.png"
 import Team9 from "../../images/team/9.png"
 import Team11 from "../../images/team/11.png"
+import Team12 from "../../images/team/12.png"
 
 import SectionBG from "../../images/our-team-bg.png"
 import SectionBGTablet from "../../images/our-team-bg-tablet.png"
@@ -27,8 +20,8 @@ import SectionBGMobile from "../../images/our-team-bg-mobile.png"
 
 import "../../styles/sections/homepage/ourTeam.scss"
 
-const OurTeamSection = () => {
-  const lottieRef = useRef();
+const ourTeamSection = () => {
+  const lottieRef = useRef()
 
   useEffect(() => {
     const buttonAnimation = Lottie.loadAnimation({
@@ -37,28 +30,28 @@ const OurTeamSection = () => {
       loop: false,
       autoplay: false,
       animationData,
-    });
+    })
 
-    buttonAnimation.setSpeed(1.15);
+    buttonAnimation.setSpeed(1.15)
 
     const handleMouseEnter = () => {
-      buttonAnimation.setDirection(1);
-      buttonAnimation.play();
-    };
+      buttonAnimation.setDirection(1)
+      buttonAnimation.play()
+    }
 
     const handleMouseLeave = () => {
-      buttonAnimation.setDirection(-1);
-      buttonAnimation.play();
-    };
+      buttonAnimation.setDirection(-1)
+      buttonAnimation.play()
+    }
 
-    lottieRef.current.addEventListener("mouseenter", handleMouseEnter);
-    lottieRef.current.addEventListener("mouseleave", handleMouseLeave);
+    lottieRef.current.addEventListener("mouseenter", handleMouseEnter)
+    lottieRef.current.addEventListener("mouseleave", handleMouseLeave)
 
     return () => {
-      lottieRef.current.removeEventListener("mouseenter", handleMouseEnter);
-      lottieRef.current.removeEventListener("mouseleave", handleMouseLeave);
-    };
-  }, []);
+      lottieRef.current.removeEventListener("mouseenter", handleMouseEnter)
+      lottieRef.current.removeEventListener("mouseleave", handleMouseLeave)
+    }
+  }, [])
 
   return (
     <section className="our-team-section">
@@ -83,23 +76,20 @@ const OurTeamSection = () => {
             </div>
             <div className="btn-wrap">
               <Link
-                to="#"
+                to="https://cryptocurrencyjobs.co/startups/apwine/"
                 className="hover-container-wrap"
-                // onMouseEnter={handleMouseEnter()}
-                // onMouseLeave={handleMouseLeave}
                 ref={lottieRef}
               >
-                <div className="hover-container"></div>
+                Join The Team
               </Link>
             </div>
           </div>
-<<<<<<< HEAD
           <div className="col">
             <PersonTile
               type="Protocol"
               personName="Gaspard Peduzzi"
-              position="Solidity Developer"
-              twitterURL="#"
+              position="Protocol"
+              twitterURL="https://twitter.com/gaspardpeduzzi"
               imageURL={Team1}
             />
           </div>
@@ -107,8 +97,8 @@ const OurTeamSection = () => {
             <PersonTile
               type="Development"
               personName="Ulysse Ramage"
-              position="Full Stack Developer"
-              twitterURL="#"
+              position="UI/UX"
+              twitterURL="https://twitter.com/ulydev"
               imageURL={Team5}
             />
           </div>
@@ -117,7 +107,7 @@ const OurTeamSection = () => {
               type="Development"
               personName="Jean Chambras"
               position="Full Stack Developer"
-              twitterURL="#"
+              twitterURL="https://twitter.com/jean_chambras"
               imageURL={Team8}
             />
           </div>
@@ -126,17 +116,35 @@ const OurTeamSection = () => {
               type="Strategic management"
               personName="Antoine Mouran"
               position="Business Developer"
-              twitterURL="#"
+              twitterURL="https://twitter.com/L0GYKAL"
               imageURL={Team4}
             />
           </div>
           <div className="col">
             <PersonTile
-              type="Development"
-              personName="Daniel Tok"
-              position="Full Stack Developer"
-              twitterURL="#"
-              imageURL={Team2}
+              type="Growth"
+              personName="Irene"
+              position="Head of Growth / Events"
+              twitterURL="https://twitter.com/irene_fucile"
+              imageURL={Team11}
+            />
+          </div>
+          <div className="col">
+            <PersonTile
+              type="Marketing"
+              personName="Red"
+              position="Marketing"
+              twitterURL="https://twitter.com/smokatokey"
+              imageURL={Team7}
+            />
+          </div>
+          <div className="col">
+            <PersonTile
+              type="Marketing"
+              personName="Sam"
+              position="Community & Internal orga."
+              twitterURL="https://twitter.com/Sam48152259"
+              imageURL={Team3}
             />
           </div>
           <div className="col">
@@ -150,20 +158,11 @@ const OurTeamSection = () => {
           </div>
           <div className="col">
             <PersonTile
-              type="Marketing"
-              personName="Red"
-              position="Marketing"
+              type="Development"
+              personName="Daniel Tok"
+              position="Full Stack Developer"
               twitterURL="#"
-              imageURL={Team7}
-            />
-          </div>
-          <div className="col">
-            <PersonTile
-              type="Marketing"
-              personName="Sam"
-              position="Community & Internal orga."
-              twitterURL="#"
-              imageURL={Team3}
+              imageURL={Team2}
             />
           </div>
           <div className="col">
@@ -177,130 +176,17 @@ const OurTeamSection = () => {
           </div>
           <div className="col">
             <PersonTile
-              type="Marketing"
-              personName="Dan"
-              position="Content Writer"
+              type="Protocol"
+              personName="Manmeet"
+              position="Solidity Developer"
               twitterURL="#"
-              imageURL={Team10}
-            />
-          </div>
-          <div className="col">
-            <PersonTile
-              type="Growth"
-              personName="Irene"
-              position="Head of Growth / Events"
-              twitterURL="#"
-              imageURL={Team11}
+              imageURL={Team12}
             />
           </div>
         </div>
       </div>
     </section>
-  );
-};
-
-export default OurTeamSection;
-=======
-          <div className="btn-wrap">
-            <Link to="https://cryptocurrencyjobs.co/startups/apwine/">
-              Join The Team
-            </Link>
-          </div>
-        </div>
-        <div className="col">
-          <PersonTile
-            type="Protocol"
-            personName="Gaspard Peduzzi"
-            position="Protocol"
-            twitterURL="https://twitter.com/gaspardpeduzzi"
-            imageURL={Team1}
-          />
-        </div>
-        <div className="col">
-          <PersonTile
-            type="Development"
-            personName="Ulysse Ramage"
-            position="UI/UX"
-            twitterURL="https://twitter.com/ulydev"
-            imageURL={Team5}
-          />
-        </div>
-        <div className="col">
-          <PersonTile
-            type="Development"
-            personName="Jean Chambras"
-            position="Full Stack Developer"
-            twitterURL="https://twitter.com/jean_chambras"
-            imageURL={Team8}
-          />
-        </div>
-        <div className="col">
-          <PersonTile
-            type="Strategic management"
-            personName="Antoine Mouran"
-            position="Business Developer"
-            twitterURL="https://twitter.com/L0GYKAL"
-            imageURL={Team4}
-          />
-        </div>
-        <div className="col">
-          <PersonTile
-            type="Growth"
-            personName="Irene"
-            position="Head of Growth / Events"
-            twitterURL="https://twitter.com/irene_fucile"
-            imageURL={Team11}
-          />
-        </div>
-        <div className="col">
-          <PersonTile
-            type="Marketing"
-            personName="Red"
-            position="Marketing"
-            twitterURL="https://twitter.com/smokatokey"
-            imageURL={Team7}
-          />
-        </div>
-        <div className="col">
-          <PersonTile
-            type="Marketing"
-            personName="Sam"
-            position="Community & Internal orga."
-            twitterURL="https://twitter.com/Sam48152259"
-            imageURL={Team3}
-          />
-        </div>
-        <div className="col">
-          <PersonTile
-            type="Protocol"
-            personName="Ayush Tiwari"
-            position="Solidity Developer"
-            twitterURL="#"
-            imageURL={Team6}
-          />
-        </div>
-        <div className="col">
-          <PersonTile
-            type="Development"
-            personName="Daniel Tok"
-            position="Full Stack Developer"
-            twitterURL="#"
-            imageURL={Team2}
-          />
-        </div>
-        <div className="col">
-          <PersonTile
-            type="Development"
-            personName="Mateusz Franczuk"
-            position="Full Stack Developer"
-            twitterURL="#"
-            imageURL={Team9}
-          />
-        </div>
-      </div>
-    </div>
-  </section>
-)
+  )
+}
 
 export default ourTeamSection
->>>>>>> 45a0528367ddb358e3fccc2f355a0f5a5042541f
