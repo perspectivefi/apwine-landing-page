dc024fb78d27840c05ff24b0fe8069fb7a89b898
Mateusz Franczuk <mat.franczuk@gmail.com>
2022-06-17 16:59:20 +0200

    fix: remove team members photos

--- src/components/personTile.js
+++ src/components/personTile.js
@@ -1,36 +1,31 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from "react"
+import { Link } from "gatsby"
 
-import Tw from "../images/socials/tw.svg";
-import Bg from "../images/person-hover-bg.svg";
+import Tw from "../images/socials/tw.svg"
 
-import "../styles/components/personTile.scss";
+import "../styles/components/personTile.scss"
 
-const PersonTile = (props) => {
+const PersonTile = props => {
   return (
-    <div className="person-tile">
-      <div className="image">
-        <img src={props.imageURL} alt={props.personName} />
-        <div className="hover-tile">
-          <div className="bg">
-            <Bg />
-          </div>
-          <Link to={props.twitterURL}>
-            <Tw />
-          </Link>
-        </div>
+    <div className={`person-tile`}>
+      <div>
+        {props.type && (
+          <div className="position-type">{props.type}</div>
+        )}
+        {props.personName && (
+          <div className="name">{props.personName}</div>
+        )}
+        {props.position && (
+          <div className="position">{props.position}</div>
+        )}
       </div>
-      {props.type.length > 0 && (
-        <div className="position-type">{props.type}</div>
-      )}
-      {props.personName.length > 0 && (
-        <div className="name">{props.personName}</div>
-      )}
-      {props.position.length > 0 && (
-        <div className="position">{props.position}</div>
-      )}
+      {props.twitterURL && <div className="twitter-tile">
+        <Link target="_blank" to={props.twitterURL}>
+          <Tw /> @{props.twitterURL.split("com/")[1]}
+        </Link>
+      </div>}
     </div>
-  );
-};
+  )
+}
 
-export default PersonTile;
+export default PersonTile
